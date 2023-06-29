7241689f54994cac13256fbeb5e827e2a5ef28e5
david almeida <contato.davidalmeida@outlook.com>
2023-06-29 11:42:49 -0300

    ✅ Update: Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,122 +1,161 @@
+import {
+	FiCloudRain,
+	FiDroplet,
+	FiSun,
+	FiSunrise,
+	FiSunset,
+	FiThermometer,
+	FiTrendingDown,
+	FiTrendingUp,
+	FiWind,
+} from "react-icons/fi";
+import { MdWaves } from "react-icons/md";
 import { Header } from "../../components/layout/Header/";
+import { Main } from "../../components/layout/Main";
 import "./Home.style.js";
 
 export const Home = () => {
 	return (
 		<>
 			<Header />
-			<main className="main">
-				<div className="container">
-					<div className="content">
-						<div className="climate_information">
-							<div className="flex">
-								<div className="location">Localização</div>
-								<div className="temperature">Temperatura</div>
-								<div className="preview">
+			<Main>
+				<div className="content">
+					<div className="main_climate_information">
+						<div className="flex-col justify-content align-items gap-3">
+							<div className="location">São Paulo, São Paulo, Brasil</div>
+							<div className="temperature">Temperatura ºC</div>
+							<div className="preview flex-row gap-1">
+								<div className="flex-row gap-1">
 									<span>
-										<div className="icon">icone</div>
-										mínima
+										<FiTrendingDown />
 									</span>
-									<span>|</span>
+									<span>mínima</span>
+								</div>
+								<div>|</div>
+								<div className="flex-row gap-1">
 									<span>
-										<div className="icon">icone</div>
-										máxima
+										<FiTrendingUp />
 									</span>
-								</div>
-							</div>
-							<div className="flex">
-								<div className="icon">icone</div>
-								<span>descrição</span>
-							</div>
-						</div>
-						<div className="climate_details">
-							<div className="climate_item">
-								<div className="icone">Icone</div>
-								<span className="index">Dados</span>
-								<span className="description">descrição</span>
-							</div>
-							<div className="climate_item">
-								<div className="icone">Icone</div>
-								<span className="index">Dados</span>
-								<span className="description">descrição</span>
-							</div>
-							<div className="climate_item">
-								<div className="icone">Icone</div>
-								<span className="index">Dados</span>
-								<span className="description">descrição</span>
-							</div>
-							<div className="climate_item">
-								<div className="icone">Icone</div>
-								<span className="index">Dados</span>
-								<span className="description">descrição</span>
-							</div>
-							<div className="climate_item">
-								<div className="icone">Icone</div>
-								<span className="index">Dados</span>
-								<span className="description">descrição</span>
-							</div>
-							<div className="climate_item">
-								<div className="icone">Icone</div>
-								<span className="index">Dados</span>
-								<span className="description">descrição</span>
-							</div>
-						</div>
-						<div className="climate_preview">
-							<div className="header">
-								<span className="today">Hoje</span>
-								<button className="toogle">Próximos 7 dias</button>
-							</div>
-							<div className="details">
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
-								</div>
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
-								</div>
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
-								</div>
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
-								</div>
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
-								</div>
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
-								</div>
-								<div className="item">
-									<div className="hour">00h00</div>
-									<div className="icon">Icone</div>
-									<div className="description">Descrição</div>
+									<span>máxima</span>
 								</div>
 							</div>
 						</div>
-						<div className="sun_moon_toogle">
-							<div className="sunrise">
-								<div className="icon">Icone</div>
-								<div className="hour">00h00</div>
+						<div className="flex-col justify-content align-items gap-3">
+							<div className="icon">icone</div>
+							<span>descrição</span>
+						</div>
+					</div>
+					<div className="main_climate_details gap-1">
+						<div className="climate_item">
+							<span>
+								<FiWind size={24} />
+							</span>
+							<p className="text text_data">Dados</p>
+							<p className="text text_description">Qualidade do ar</p>
+						</div>
+						<div className="climate_item">
+							<span>
+								<FiThermometer size={24} />
+							</span>
+							<span className="text text_data">Dados</span>
+							<span className="text text_description">Pressão ATM</span>
+						</div>
+						<div className="climate_item">
+							<span>
+								<FiSun size={24} />
+							</span>
+							<span className="text text_data">Dados</span>
+							<span className="text text_description">Raios UV</span>
+						</div>
+						<div className="climate_item">
+							<span>
+								<FiCloudRain size={24} />
+							</span>
+							<span className="text text_data">Dados</span>
+							<span className="text text_description">
+								Umidade relativa do ar
+							</span>
+						</div>
+						<div className="climate_item">
+							<span>
+								<MdWaves size={24} />
+							</span>
+							<span className="text text_data">Dados</span>
+							<span className="text text_description">Veloc. do vento</span>
+						</div>
+						<div className="climate_item">
+							<span>
+								<FiDroplet size={24} />
+							</span>
+							<span className="text text_data">Dados</span>
+							<span className="text text_description">Volume de chuva</span>
+						</div>
+					</div>
+					<div className="main_climate_preview">
+						<div className="content">
+							<div className="climate_preview_title">
+								<p className="text text--md">Hoje</p>
+								<button className="button button--text text text--md">
+									Próximos 7 dias
+								</button>
 							</div>
-							<div className="sunset">
-								<div className="icon">Icone</div>
-								<div className="hour">05h00</div>
+							<div className="climate_preview_details">
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
+								<div className="climate_item">
+									<div className="text">00h00</div>
+									<div className="text">Icone</div>
+									<div className="text">Descrição</div>
+								</div>
 							</div>
 						</div>
 					</div>
+					<div className="main_sun_moon_toggle">
+						<div className="time_container">
+							<span>
+								<FiSunrise size={24} />
+							</span>
+							<p className="text">Nascer do Sol</p>
+							<p className="text">00h00</p>
+						</div>
+						<div className="time_container">
+							<span>
+								<FiSunset size={24} />
+							</span>
+							<p className="text">Pôr do Sol</p>
+							<p className="text">05h00</p>
+						</div>
+					</div>
 				</div>
-			</main>
+			</Main>
 		</>
 	);
 };
