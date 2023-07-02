54ece98c8e218a6fd31c413254fbc8398bd75376
david almeida <contato.davidalmeida@outlook.com>
2023-07-02 20:51:19 -0300

    💄 Add or update the UI and style files

--- src/components/layout/Main/Main.style.js
+++ src/components/layout/Main/Main.style.js
@@ -1,30 +1,5 @@
 import { styled } from "styled-components";
 
 export const StyledMain = styled.main`
-	.content {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-	}
-
-	.main_sun_moon_toggle {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		gap: 0.5rem;
-		box-shadow: var(--shadow-md);
-		border-radius: var(--rounded-lg);
-		padding: 1rem 0;
-
-		@media (min-width: 768px) {
-			width: 288px;
-		}
-
-		.time_container {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			gap: 4px;
-		}
-	}
+	background-color: var(--white);
 `;
