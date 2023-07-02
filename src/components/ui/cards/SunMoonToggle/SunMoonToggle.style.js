52af6f863ae41bb94e0289b117e614a39d503463
david almeida <contato.davidalmeida@outlook.com>
2023-07-02 20:49:41 -0300

    ✅ Update: SunMoonToggle component

--- src/components/ui/cards/SunMoonToggle/SunMoonToggle.style.js
+++ src/components/ui/cards/SunMoonToggle/SunMoonToggle.style.js
@@ -1,3 +1,71 @@
 import { styled } from "styled-components";
 
-export const StyledSunMoonToggle = styled.div``;
+export const StyledSunMoonToggle = styled.div`
+	background-color: var(--primary-color);
+	border-radius: var(--rounded-md);
+	color: var(--white);
+
+	.astro__content--glassmorphism {
+		background: rgba(255, 255, 255, 0.2);
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		backdrop-filter: blur(2px);
+		-webkit-backdrop-filter: blur(2px);
+		padding: 1rem;
+
+	}
+
+	.astro__title {
+		font-size: var(--text-lg);
+		line-height: var(--line-height-lg);
+		font-family: var(--fontFamilyLato);
+		background: rgba(255, 255, 255, 0.2);
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		backdrop-filter: blur(2px);
+		-webkit-backdrop-filter: blur(2px);
+		border-radius: var(--rounded-md);
+		padding: 1rem;
+	}
+
+	.astro__content {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		padding: 1rem;
+
+		.astro__info {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: center;
+			gap: 0.125rem;
+		}
+
+		.astro__info--row {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+		}
+
+		.astro__text {
+			font-family: var(--fontFamilyLato);
+			font-size: var(--text-md);
+			line-height: var(--line-height-md);
+		}
+
+		.astro__icon {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+
+		.astro__moon-phase {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			gap: 0.125rem;
+		}
+	}
+`;
--- src/components/ui/cards/SunMoonToggle/SunMoonToggle.tsx
+++ src/components/ui/cards/SunMoonToggle/SunMoonToggle.tsx
@@ -1,35 +1,88 @@
-import { FiSunrise, FiSunset } from "react-icons/fi";
 import { StyledSunMoonToggle } from "./SunMoonToggle.style";
 import { ForecastWeatherData } from "../../../../interfaces/ForecastWeatherData";
 import { formatDateToYYYYMMDD } from "../../../utils/formatDateToYYYYMMDD";
+import { convertTo24HourFormat } from "../../../utils/hourConvertFormat";
+import {
+	WiMoonrise,
+	WiMoonset,
+	WiNightClear,
+	WiSunrise,
+	WiSunset,
+} from "react-icons/wi";
+import { FiChevronRight } from "react-icons/fi";
 
 export const SunMoonToggle = ({ data }: ForecastWeatherData) => {
-	const { forecastday } = data.forecast;
-	// const currentDay = new Date()
-	// const currentDayFormatted = formatDateToYYYYMMDD(currentDay)
-	// console.log(forecastday)
+	const currentDate = formatDateToYYYYMMDD(new Date());
+	const forecastDayFiltered = data.forecast.forecastday.filter(
+		(element) => element.date === currentDate,
+	)[0];
 
-	// forecastday.map((element, index) => {
-	//   console.log(element.date, currentDayFormatted)
-	//   console.log(element.date === currentDayFormatted)
-	// })
+	const { astro } = forecastDayFiltered;
+
+	const convertedSunrise = convertTo24HourFormat(astro.sunrise);
+	const convertedSunset = convertTo24HourFormat(astro.sunset);
+	const convertedMoonrise = convertTo24HourFormat(astro.moonrise);
+	const convertedMoonset = convertTo24HourFormat(astro.moonset);
+	const currentHour = new Date().getHours().toString();
 
 	return (
 		<StyledSunMoonToggle>
-			<div className="time_container">
-				<span>
-					<FiSunrise size={24} />
-				</span>
-				<p className="text">Nascer do Sol</p>
-				<p className="text">00h00</p>
-			</div>
-			<div className="time_container">
-				<span>
-					<FiSunset size={24} />
-				</span>
-				<p className="text">Pôr do Sol</p>
-				<p className="text">05h00</p>
-			</div>
+			{convertedSunrise <= currentHour && currentHour <= convertedSunset ? (
+				<>
+					<div className="astro__content--glassmorphism">
+						<h3 className="astro__title">Nascer e pôr</h3>
+					</div>
+					<div className="astro__content">
+						<div className="astro__info">
+							<span className="astro__icon">
+								<WiSunrise size={24} />
+							</span>
+							<p className="astro__text">{convertedSunrise}</p>
+						</div>
+						<div className="astro__info">
+							<span className="astro__icon">
+								<WiSunset size={24} />
+							</span>
+							<p className="astro__text">{convertedSunset}</p>
+						</div>
+					</div>
+				</>
+			) : (
+				<>
+					<h3 className="astro__title">Nascer e pôr</h3>
+					<div className="astro__content">
+						<div className="astro__info">
+							<span className="astro__icon">
+								<WiMoonrise size={24} />
+							</span>
+							<p className="astro__text">{convertedMoonrise}</p>
+						</div>
+						<div className="astro__moon-phase">
+							<div className="astro__info--row">
+								<span className="astro__icon">
+									<WiNightClear size={24} />
+								</span>
+								<p className="astro__text">{astro.moon_phase}</p>
+							</div>
+							<div className="astro__info--row">
+								<p className="astro__text">Luz refletida</p>
+								<span className="astro__icon">
+									<FiChevronRight />
+								</span>
+								<p className="astro__text">{astro.moon_illumination}</p>
+							</div>
+						</div>
+						<div className="astro__info">
+							<span>
+								<WiMoonset size={24} />
+							</span>
+							<p className="astro__text">{convertedMoonset}</p>
+						</div>
+					</div>
+				</>
+			)}
 		</StyledSunMoonToggle>
 	);
 };
+
+export default SunMoonToggle;
