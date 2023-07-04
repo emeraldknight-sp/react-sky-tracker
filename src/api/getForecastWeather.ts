6d7bb7dcb3f01e0b341927939872a6d1981f10d8
david almeida <contato.davidalmeida@outlook.com>
2023-07-03 21:21:32 -0300

    ✅ Update: GetData Functions

--- src/api/getForecastWeather.ts
+++ src/api/getForecastWeather.ts
@@ -1,20 +1,19 @@
+import axios from "axios";
+
 import { locationNameFormat } from "../components/utils/locationFormat";
 import { GeoLocationData } from "../interfaces/GeoLocationData";
-const locationString = "Teresina";
 
 export const getForecastWeatherData = async (props: GeoLocationData) => {
-	const location = locationNameFormat(props, "string") || locationString;
+	const location = locationNameFormat(props, "string") || "Teresina";
 
 	try {
-		const res = await fetch(
+		const { data } = await axios.get(
 			`https://api.weatherapi.com/v1/forecast.json?key=f2345c34a365497db90123644230106&q=${location}&days=yes&aqi=yes&alerts=yes`,
 		);
-		const data = await res.json();
-
-		console.log("FORECAST:", data)
 
 		return data;
-	} catch (err) {
-		console.error("Erro: ", err);
+	} catch (error) {
+		console.error("Erro: ", error);
+		throw error;
 	}
 };
