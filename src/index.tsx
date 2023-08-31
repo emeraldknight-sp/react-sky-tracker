a3885535350732a3a33c6e57a5a929dcc28a54db
David <contato.davidalmeida@outlook.com>
2023-08-31 14:26:43 -0300

    ✅ Update: Index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { App } from "./App";
 import { GlobalStyles } from "./styles/global";
 import { Metric } from "web-vitals";
 import { reportWebVitals } from "./reportWebVitals";
+import { LocationProvider } from "./context/LocationContext";
+import { ForecastWeatherProvider } from "./context/ForecastWeatherContext";
+import { CurrentWeatherProvider } from "./context/CurrentWeatherContext";
 
 const rootElement = document.getElementById("root");
 const handleWebVitals = (metric: Metric) => {
@@ -15,8 +18,14 @@ if (rootElement) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
 		<React.StrictMode>
-			<GlobalStyles />
-			<App />
+			<LocationProvider>
+				<ForecastWeatherProvider>
+					<CurrentWeatherProvider>
+						<GlobalStyles />
+						<App />
+					</CurrentWeatherProvider>
+				</ForecastWeatherProvider>
+			</LocationProvider>
 		</React.StrictMode>,
 	);
 } else {
