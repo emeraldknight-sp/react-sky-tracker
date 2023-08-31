b9aa0dba392def7604b60d94acac9aedea738d63
David <contato.davidalmeida@outlook.com>
2023-08-31 15:28:05 -0300

    ✅ Update: Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,24 +9,13 @@ import { ClimateDetails } from "../../components/ui/cards/ClimateDetails";
 import { ClimatePreview } from "../../components/ui/cards/ClimatePreview";
 import { SunMoonToggle } from "../../components/ui/cards/SunMoonToggle";
 
-import { mockForecast } from "../../mock/mockForecast";
-import { mockCurrent } from "../../mock/mockCurrent";
-
 import { LocationContext } from "../../context/LocationContext";
-import { ForecastWeatherContext } from "../../context/ForecastWeatherContext";
-import { CurrentWeatherContext } from "../../context/CurrentWeatherContext";
 
 import { ElementGroupHome, StyledHome } from "./Home.style";
 import { Loading } from "../../components/layout/Loading";
 
 export const Home = () => {
 	const location = useContext(LocationContext);
-	const forecastWeather = useContext(ForecastWeatherContext);
-	const currentWeather = useContext(CurrentWeatherContext);
-
-	console.log("LOCATION", location);
-	console.log("FORECAST", forecastWeather);
-	console.log("CURRENT", currentWeather);
 
 	return (
 		<StyledHome>
@@ -35,16 +24,16 @@ export const Home = () => {
 				{location ? (
 					<ElementGroupHome>
 						<div className="content--box-1">
-							<ClimateInformation data={mockForecast} />
+							<ClimateInformation />
 						</div>
 						<div className="content--box-2">
-							<ClimateDetails data={mockCurrent} />
+							<ClimateDetails />
 						</div>
 						<div className="content--box-3">
-							<ClimatePreview data={mockForecast} />
+							<ClimatePreview />
 						</div>
 						<div className="content--box-4">
-							<SunMoonToggle data={mockForecast} />
+							<SunMoonToggle />
 						</div>
 					</ElementGroupHome>
 				) : (
