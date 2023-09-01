740718690d4e27896cd8795ab40bf8836b824765
David <contato.davidalmeida@outlook.com>
2023-09-01 08:48:22 -0300

    ✅ Add: Button component

--- src/components/ui/Button/Button.style.ts
+++ src/components/ui/Button/Button.style.ts
@@ -2,14 +2,39 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
 	padding: 0.5rem 1rem;
-	background-color: var(--button-bg-color, #007bff);
-	color: var(--button-text-color, #fff);
-	border: none;
-	border-radius: 5px;
-	cursor: pointer;
-	transition: background-color 0.3s ease;
 
-	&:hover {
-		background-color: var(--button-hover-bg-color, #0056b3);
+	border: 3px solid var(--transparent);
+	border-radius: 8px;
+	cursor: pointer;
+
+	outline: none;
+
+	font-family: var(--fontFamilyLato);
+	font-size: var(--text-md);
+	line-height: var(--line-height-md);
+	font-weight: 600;
+
+	&.button--contained {
+		background-color: var(--primary-color, #007bff);
+		color: var(--white, #fff);
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+		transition: background-color 0.3s ease;
+
+		&:hover {
+			background-color: var(--charcoal, #0056b3);
+		}
+	}
+
+	&.button--text {
+		background-color: var(--transparent);
+		transition: all 0.3s ease;
+		color: var(--charcoal);
+
+		&:hover {
+			color: var(--primary-color);
+		}
+	}
+
+	&.button--outlined {
 	}
 `;
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,10 +1,27 @@
 import { StyledButton } from "./Button.style";
 
+interface ButtonProps {
+	children: React.ReactNode;
+	onClick?: () => void;
+	type?: "button" | "reset" | "submit";
+	style: "contained" | "text" | "outlined";
+	size: "xs" | "sm" | "md" | "lg";
+	className?: string;
+}
+
 export const Button = ({
 	children,
-	...rest
-}: {
-	children: React.ReactNode;
-}) => {
-	return <StyledButton {...rest}>{children}</StyledButton>;
+	type = "button",
+	style = "contained",
+	size = "md",
+	onClick,
+	className,
+}: ButtonProps) => {
+	const buttonClassName = `button button--${size} button--${style} ${className}`;
+
+	return (
+		<StyledButton type={type} className={buttonClassName} onClick={onClick}>
+			{children}
+		</StyledButton>
+	);
 };
