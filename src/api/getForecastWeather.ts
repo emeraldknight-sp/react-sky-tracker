1dc22bcb0dc34eca0a9ddab2a02e347dcab92211
David <contato.davidalmeida@outlook.com>
2023-09-01 14:25:46 -0300

    ✅ Update: getForecastWeather function

--- src/api/getForecastWeather.ts
+++ src/api/getForecastWeather.ts
@@ -5,10 +5,11 @@ import { GeoLocation } from "../interfaces/GeoLocation";
 
 export const getForecastWeatherData = async (props: GeoLocation) => {
 	const location = locationNameFormat(props, "string") || "Teresina";
+	const language = "pt"
 
 	try {
 		const { data } = await axios.get(
-			`https://api.weatherapi.com/v1/forecast.json?key=f2345c34a365497db90123644230106&q=${location}&days=yes&aqi=yes&alerts=yes`,
+			`https://api.weatherapi.com/v1/forecast.json?key=f2345c34a365497db90123644230106&q=${location}&days=10&aqi=yes&alerts=yes&lang=${language}&tides=yes`,
 		);
 
 		return data;
