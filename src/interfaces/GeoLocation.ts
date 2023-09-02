580bcae7516af5b838417bbe04b89e2515b19069
David <contato.davidalmeida@outlook.com>
2023-09-02 08:57:31 -0300

    🏷️ Add or update types

--- src/interfaces/GeoLocation.ts
+++ src/interfaces/GeoLocation.ts
@@ -1,52 +1,17 @@
 export interface GeoLocation {
-	city: {
-		name: string;
-		names: {
-			[key: string]: string;
-		};
-	};
-	continent: {
-		code: string;
-		geoname_id: number;
-		names: {
-			[key: string]: string;
-		};
-		name: string;
-	};
-	country: {
-		geoname_id: number;
-		iso_code: string;
-		names: {
-			[key: string]: string;
-		};
-		name: string;
-		name_native: string;
-		phone_code: string;
-		capital: string;
-		currency: string;
-		flag: string;
-		languages: {
-			iso_code: string;
-			name: string;
-			name_native: string;
-		}[];
-	};
-	location: {
-		latitude: number;
-		longitude: number;
-	};
-	subdivisions: {
-		names: {
-			[key: string]: string;
-		};
-	}[];
-	state: {
-		name: string;
-	};
-	datasource: {
-		name: string;
-		attribution: string;
-		license: string;
-	}[];
 	ip: string;
+	type: string;
+	continent_code: string;
+	continent_name: string;
+	country_code: string;
+	country_name: string;
+	is_eu: string;
+	geoname_id: number;
+	city: string;
+	region: string;
+	lat: number;
+	lon: number;
+	tz_id: string;
+	localtime_epoch: number;
+	localtime: string;
 }
