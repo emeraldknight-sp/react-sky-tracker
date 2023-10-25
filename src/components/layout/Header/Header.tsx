617bcbb7018835766af6c7f977637cf479093f76
David <contato.davidalmeida@outlook.com>
2023-10-25 15:19:10 -0300

    ✅ Update: Header component

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FiBell } from "react-icons/fi";
+// import { FiBell } from "react-icons/fi";
 import { Logo } from "../../ui/Logo";
 import { HeaderContainer, StyledHeader } from "./Header.style";
 import { Menu } from "../../ui/Menu";
@@ -6,13 +6,13 @@ import { Menu } from "../../ui/Menu";
 export const Header = () => {
 	return (
 		<StyledHeader>
-				<HeaderContainer>
-					<Logo />
-					<Menu />
-					<button className="header__button__icon">
+			<HeaderContainer>
+				<Logo />
+				<Menu />
+				{/* <button className="header__button__icon">
 						<FiBell size={20} />
-					</button>
-				</HeaderContainer>
+					</button> */}
+			</HeaderContainer>
 		</StyledHeader>
 	);
 };
