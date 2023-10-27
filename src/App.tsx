245a43bc5102cef85283bd99b02ab396017205bd
David <contato.davidalmeida@outlook.com>
2023-10-27 16:41:33 -0300

    ✅ Update: App and index files

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import {
 	RouteObject,
 	createBrowserRouter,
 } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
+import { Home } from "./pages/Home";
+import { Account } from "./pages/Account";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 const routes: RouteObject[] = [
 	{
 		path: "/",
 		element: <Home />,
+	},
+	{
+		path: "/account",
+		element: <Account />,
+	},
+	{
+		path: "/register",
+		element: <Register />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
 	},
 ];
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { App } from "./App";
+import { ContextProvider } from "./context/index";
 import { GlobalStyles } from "./styles/global";
 import { Metric } from "web-vitals";
-import { reportWebVitals } from "./reportWebVitals";
-import { LocationProvider } from "./context/LocationContext";
-import { ForecastWeatherProvider } from "./context/ForecastWeatherContext";
-import { CurrentWeatherProvider } from "./context/CurrentWeatherContext";
 import { Toaster } from "react-hot-toast";
+import { reportWebVitals } from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 const handleWebVitals = (metric: Metric) => {
@@ -19,15 +16,11 @@ if (rootElement) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
 		<React.StrictMode>
-			<LocationProvider>
-				<ForecastWeatherProvider>
-					<CurrentWeatherProvider>
-						<Toaster position="bottom-right" reverseOrder={false} />
-						<GlobalStyles />
-						<App />
-					</CurrentWeatherProvider>
-				</ForecastWeatherProvider>
-			</LocationProvider>
+			<ContextProvider>
+				<Toaster position="bottom-right" reverseOrder={false} />
+				<GlobalStyles />
+				<App />
+			</ContextProvider>
 		</React.StrictMode>,
 	);
 } else {
