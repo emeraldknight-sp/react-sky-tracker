65212905a7c355e304951681167495b09a36dd57
David <contato.davidalmeida@outlook.com>
2023-10-29 16:04:32 -0300

    ✅ Update: User data mock

--- src/mock/mockUser.ts
+++ src/mock/mockUser.ts
@@ -1,7 +1,28 @@
-export const mockUser = {
-	firstName: "",
-	lastName: "",
+import { User } from "../interfaces/User";
+
+export const mockUser: User = {
+	id: "",
+	username: "",
 	email: "",
 	password: "",
 	confirmPassword: "",
+	firstName: "",
+	lastName: "",
+	location: {
+		latitude: 0,
+		longitude: 0,
+		city: "",
+		country: "",
+		timezone: "",
+	},
+	settings: {
+		units: "metric",
+		notifications: {
+			previsaoDiaria: false,
+			alertasMeteorologicos: false,
+		},
+		theme: "light",
+	},
+	favorites: [],
+	searchHistory: [],
 };
